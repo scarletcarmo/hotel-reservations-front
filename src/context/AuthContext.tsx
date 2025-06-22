3294fcab4b005db4328a7d2c0d86087d5fbfcca6
Andres9792 <andresxavymg4@gmail.com>
2025-06-21 19:30:09 -0500

    login was complete

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import { login as loginService } from '../api/authService';
+import { LoginResponse } from '../types/Auth'
+
+interface AuthContextType {
+  user: LoginResponse['user'] | null;
+  token: string | null;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<LoginResponse['user'] | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    const storedUser = localStorage.getItem('user');
+    if (storedToken && storedUser) {
+      setToken(storedToken);
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = async (email: string, password: string) => {
+    const data = await loginService(email, password);
+    setToken(data.token);
+    setUser(data.user);
+    localStorage.setItem('token', data.token);
+    localStorage.setItem('user', JSON.stringify(data.user));
+  };
+
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuthContext debe usarse dentro de un AuthProvider');
+  }
+  return context;
+};
